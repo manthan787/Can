a631b37a618e93bc991c1751ff0d6d1db8712c24
Manthan787 <manthant15@gmail.com>
2014-10-16 17:48:46 +0530

    Bug Fixes

--- app/views/emails/contact.blade.php
+++ app/views/emails/contact.blade.php
@@ -1,5 +1,6 @@
+<h3>
 You have received a new contact Us Message from a customer. The details are as follows:<br>	
-
+</h3>
 <br><br>
 	<strong>Name</strong>:{{ $name }} <br>
 	<strong>Email</strong>:{{ $email }}<br>
