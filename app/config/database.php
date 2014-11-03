7cec43c54b753137f489e8df9bcb8713ae0b6095
Manthan787 <manthant15@gmail.com>
2014-11-04 01:06:39 +0530

    fav

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'CandleStore',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'manthan787',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
