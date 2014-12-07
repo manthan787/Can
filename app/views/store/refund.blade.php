3b51aad188e60005615474f89d313283b755e279
manthan787 <manthant15@gmail.com>
2014-12-08 01:47:37 +0530

    Refund Policy

--- app/views/store/refund.blade.php
+++ app/views/store/refund.blade.php
@@ -17,7 +17,15 @@ Refund And Cancellation Policy
    
       <h1 class="heading1"><span class="maintext">Refund And Cancellation Policy</span></h1>
       <section id="typography"> 
-      <p>    
+      <p> 
+      <h3>When can you ask for refund?</h3>
+      <b>Case I</b>
+      <p>You can ask for refund if you don't receive the product within 10 working days from the date you placed the order.</p>
+
+      <b>Case II</b>
+      <p>You can ask for refund within 3 days after you receive the product.</p>
+
+      <p>  You will receive the refund amount within 8-10 working days after your case is approved. </p>  
       <h3>How do you receive your refund through CandleStore?</h3>
       <b>Candlestore shall refund payment by providing you Candlestore Cash for amounts less than Rs 1000. For amounts greater than Rs 1000, refunds will be made through either CandleStore Cash, cheque or bank transfer depending on the mode of payment used at the time of placing the order.</b><br>
       <h3>What does it cover?</h3>
